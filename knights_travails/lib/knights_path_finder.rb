0fc28e8bba6079d8ae0479c75629804cde9f7bc1
Steven Inouye <steveninouye@msn.com>
2018-10-26 14:29:48 -0700

    Complete KnightPathFinder::valid_moves

--- knights_travails/lib/knights_path_finder.rb
+++ knights_travails/lib/knights_path_finder.rb
@@ -2,7 +2,7 @@ require_relative "00_tree_node"
 
 class KnightPathFinder
 
-  KNIGHT_MOVES = [
+  KNIGHT_MOVES_DELTA = [
     [2,1],
     [2, -1],
     [-2,1],
@@ -26,6 +26,18 @@ class KnightPathFinder
   end
 
   def self.valid_moves(pos)
-
+    current_row, current_col = pos
+    potential_moves = []
+    KNIGHT_MOVES.each do |delta|
+      row_delta, col_delta = delta
+      new_row = current_row + row_delta
+      new_col = current_col + col_delta
+      if (new_row).between(0, 7) &&
+         (new_col).between(0, 7) &&
+         !@visited_positions.include?([new_row, new_col])
+         potential_move << [new_row, new_col]
+      end
+    end
+    potential_moves
   end
 end
