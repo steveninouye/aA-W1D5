cfe5bc2257ee81fa1e51bee7781064c168f1e5de
Steven Inouye <steveninouye@msn.com>
2018-10-26 14:14:33 -0700

    Define KnightPathFinder::valid_moves

--- knights_travails/lib/knights_path_finder.rb
+++ knights_travails/lib/knights_path_finder.rb
@@ -2,13 +2,18 @@ require_relative "00_tree_node"
 
 class KnightPathFinder
   def initialize(start_pos)
-    @start_node = PolyTreeNode.new(start_pos)
+    @root_node = PolyTreeNode.new(start_pos)
     @start_pos = start_pos
     @pos = start_pos
     @move_tree = #???
+    @visited_positions = []
   end
 
   def build_move_tree
+
+  end
+
+  def self.valid_moves(pos)
 
   end
 end
